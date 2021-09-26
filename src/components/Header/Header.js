cede50a4ea5f4ef8d6632460cda29b3cda787de7
MrHamdan <mr.hamdanahmed@gmail.com>
2021-09-26 18:46:16 +0600

    adding transition to card

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className="header">
             <h1><span className="text-color-1">You</span><span className="text-color-2">Tubers</span>       <span className="text-color-3">Analytics</span></h1>
-            <h2 className="site-detail-text-color">View the daily YouTube analytics, Make your own YouTube show with them or be their Sponsors!!!</h2>
+            <h2 className="site-detail-text-color">You can hire your favorite YouTuber for your company Brand Ambassador Or Make your own YouTube show with them and be their Sponsors!!!</h2>
             <h3><span className="text-color-1">Total</span>           <span className="text-color-2">Budget</span> <span className="text-color-3">  :150 Million</span></h3>
         </div>
     );
